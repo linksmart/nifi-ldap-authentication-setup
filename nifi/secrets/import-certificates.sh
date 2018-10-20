cca62a789288b35f39fae89dae3a597e506f31a6
Junhong Liang <junhong.liang@fit.fraunhofer.de>
2018-10-20 19:19:52 +0200

    Cleaned up import-certificate.sh

--- nifi/secrets/import-certificates.sh
+++ nifi/secrets/import-certificates.sh
@@ -6,10 +6,8 @@ PSWD=$3
 
 apk --update add openssl
 
-openssl pkcs12 -export -in ${CERT_FILE} -inkey ${KEY_FILE} -out nifi.p12 -password pass:${PSWD} -name nifi
-keytool -importkeystore -srckeystore nifi.p12 -srcstoretype PKCS12 -srcstorepass ${PSWD} -destkeystore keystore.jks -deststorepass ${PSWD}
-keytool -export -keystore keystore.jks -alias nifi -file nifi.der -storepass ${PSWD}
+openssl pkcs12 -export -in ${CERT_FILE} -inkey ${KEY_FILE} -out nifi.p12 -password "pass:${PSWD}" -name nifi
+keytool -importkeystore -srckeystore nifi.p12 -srcstoretype PKCS12 -srcstorepass "${PSWD}" -destkeystore keystore.jks -deststorepass "${PSWD}"
 chmod 640 keystore.jks
 
-rm -f nifi.der
 rm -f nifi.p12
