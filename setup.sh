0fd7c925b2ababee384cec0e0607130e3d335bc7
Junhong Liang <Junhong Liang>
2018-10-22 05:02:43 -0400

    Fixed errors; Made the setup script more interactive

--- setup.sh
+++ setup.sh
@@ -1,28 +1,10 @@
 #!/bin/bash -e
 
-#-------------------------------------------
-# Basic configuration for the LDAP root DN
-#-------------------------------------------
-DOMAIN_COMPONENTS=("fit" "fraunhofer" "de")
-OU=people
-ORGANISATION="Fraunhofer FIT"
-# This variable will be used to construct part of the image name and container name,
-# e.g. ${DOCKER_NAMESPACE}/nifi as image name, and ${DOCKER_NAMESPACE}-nifi as container name
-DOCKER_NAMESPACE="fit"
-
-#-------------------------------------------
-# LDAP Variables
-# This password is the password for default LDAP admin, which has a DN of "cn=admin,dc=<user specified>,dc=<user specified>"
-#-------------------------------------------
-LDAP_ADMIN_PASSWORD=fit
-
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Do not modify the lines below!
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-echo "==========================="
-echo "Starting Nifi - LDAP stack"
-echo "==========================="
+echo " ---------------------"
+echo "|                     |"
+echo "|  Nifi - LDAP stack  |"
+echo "|                     |"
+echo " ---------------------"
 
 # Function to generate random password
 gen_pass(){
@@ -33,26 +15,38 @@ gen_pass(){
 rm -f ./ldap/secrets/users.ldif
 rm -f ./.env
 
-# String split??
-IFS=', ' read -r -a array <<< "$string"
-
-read -p "The hostname of the machine running Nifi container:\n" NIFI_HOST
-read -p "The host's forwarded port to the Nifi UI:\n" NIFI_PORT
-
-read -p "The username of the initial admin, e.g. \"nifi-admin\":\n" NIFI_ADMIN_UID
-echo "The password of the initial admin:"
-read -s NIFI_ADMIN_PASSWORD
-
-# Basic DN calculation
-for DC in "${DOMAIN_COMPONENTS[@]}"; do
-   if [ ! -z $DOMAIN ]; then
-       DOMAIN="${DOMAIN}."
+read -p "Enter the domain, e.g. \"example.com\":" DOMAIN
+read -p "Enter the organization name, e.g. \"Example Inc.\":" ORGANISATION
+OU=system
+# DN calculation
+IFS='.' read -r -a DC_ARRAY <<< "${DOMAIN}"
+for DC in "${DC_ARRAY[@]}"; do
+   if [ ! -z $BASE_DN ]; then
        BASE_DN="${BASE_DN},"
    fi
-   DOMAIN="${DOMAIN}${DC}"
    BASE_DN="${BASE_DN}dc=${DC}"
 done
 OU_DN="ou=$OU,$BASE_DN"
+
+read -p "The hostname of the machine running Nifi container:" NIFI_HOST
+read -p "The host's forwarded port to the Nifi UI:" NIFI_PORT
+
+read -p "The username of the initial admin, e.g. \"nifi-admin\":" NIFI_ADMIN_UID
+echo "The password of the initial admin:"
+read -s NIFI_ADMIN_PASSWORD
+
+echo "The password of the default LDAP manager \"cn=admin,${BASE_DN}\" (press enter to let the script generate a random password):"
+read -s LDAP_ADMIN_PASSWORD
+if [ -z ${LDAP_ADMIN_PASSWORD} ]; then
+    echo "No password provided, generating a random password"
+    LDAP_ADMIN_PASSWORD=$(gen_pass)
+fi
+
+echo "The namespace to be used for building docker images and containers (press enter to use default namespace \"secure\"):"
+read -s DOCKER_NAMESPACE
+if [ -z ${DOCKER_NAMESPACE} ]; then
+    DOCKER_NAMESPACE=secure
+fi
 
 # Nifi env variables
 NIFI_ADMIN_DN="uid=$NIFI_ADMIN_UID,$OU_DN"
@@ -65,15 +59,16 @@ NIFI_LDAP_USER_SEARCH_BASE="$OU_DN"
 LDAP_ORGANISATION=${ORGANISATION}
 LDAP_DOMAIN=${DOMAIN}
 
-if [ ! -f ./secrets/keystore.jks ]; then
+if [ ! -f ./nifi/secrets/keystore.jks ]; then
     echo "keystore.jks does not exist. Generating a new keystore."
     read -p "Please enter the subject of cert. It typically has the form \"CN=[hostname],OU=nifi\":" SERVER_CERT_SUBJECT
-    NIFI_KEYSTORE_PASS=gen_pass
+    NIFI_KEYSTORE_PASS=$(gen_pass)
+    echo $NIFI_KEYSTORE_PASS
     echo " "
     echo "---------------------------------------------"
     echo "Generating certificate with subject field: ${SERVER_CERT_SUBJECT}"
     echo "---------------------------------------------"
-    docker run -it --rm -v "$PWD/secrets":/usr/src/secrets \
+    docker run -it --rm -v "$PWD/nifi/secrets":/usr/src/secrets \
         -w /usr/src/secrets --user ${UID} openjdk:8-alpine \
         /usr/src/secrets/generate-keystore.sh \
         "${SERVER_CERT_SUBJECT}" "${NIFI_KEYSTORE_PASS}"
@@ -81,14 +76,15 @@ if [ ! -f ./secrets/keystore.jks ]; then
     echo "Keystore generation finished!"
     echo "---------------------------------------------"
 else
-    echo -n "keystore.jks detected. Please provide the password for the the keystore: "
+    echo -n "keystore.jks detected. Please provide the password for the the keystore:"
     read -s NIFI_KEYSTORE_PASS
+    echo " "
 fi
 
-if [ ! -f ./secrets/truststore.jks ]; then
-    echo "truststore.jks does not exist. Generating new truststore.jks"
-    NIFI_TRUSTSTORE_PASS=gen_pass
-    docker run -it --rm -v "$PWD/secrets":/usr/src/secrets \
+if [ ! -f ./nifi/secrets/truststore.jks ]; then
+    echo "truststore.jks does not exist. Generating a dummy truststore.jks"
+    NIFI_TRUSTSTORE_PASS=$(gen_pass)
+    docker run -it --rm -v "$PWD/nifi/secrets":/usr/src/secrets \
         -w /usr/src/secrets --user ${UID} openjdk:8-alpine \
         /usr/src/secrets/generate-truststore.sh \
         "${NIFI_KEYSTORE_PASS}" "${NIFI_TRUSTSTORE_PASS}"
@@ -98,6 +94,7 @@ if [ ! -f ./secrets/truststore.jks ]; then
 else
     echo -n "truststore.jks detected. Please provide the password for the the truststore: "
     read -s NIFI_TRUSTSTORE_PASS
+    echo " "
 fi
 
 
@@ -155,9 +152,21 @@ echo "To start the stack, simply run:"
 echo " "
 echo "  docker-compose up"
 echo " "
-echo "Then you can visit Nifi under this URL:"
+echo "Docker will build two images with the following two names:"
+echo " "
+echo "  ${DOCKER_NAMESPACE}/nifi ${DOCKER_NAMESPACE}/ldap"
+echo " "
+echo "You need to make sure there are no existing images with these names, otherwise they won't be built."
+echo " "
+echo "Docker-compose will spin up two containers with the following names:"
+echo " "
+echo "  ${DOCKER_NAMESPACE}-nifi ${DOCKER_NAMESPACE}-ldap"
+echo " "
+echo "You need to make sure no existing container with these names. If everything goes well, you can visit Nifi under this URL:"
 echo " "
 echo "  https://${NIFI_HOST}:${NIFI_PORT}"
+echo " "
+echo "Log in with the username ${NIFI_ADMIN_UID} and the password you set earlier."
 echo " "
 echo "Happy flowing!"
 echo " "
